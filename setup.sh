5db8b028c4c12e27f6102a9ec51c604f72450bb4
Hardik Joshi <mail@hardikjoshi.me>
2016-07-20 20:34:05 +0530

    Added ctags
    Added ctags

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ apt install python-dev python-pip python3-dev python3-pip
 apt install nodejs
 apt install golang
 apt install zsh
+apt install ctags
 apt install vim vim-gtk nvim 
 chsh -s /bin/zsh
 zsh
